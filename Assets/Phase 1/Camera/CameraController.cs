81c2372a3a23c3ea8dbba441d83b12bd41239b2a
Alex Miller <alex.miller@readify.net>
2021-06-28 20:17:29 +1000

    Zoom Builder

--- Assets/Phase 1/Camera/CameraController.cs
+++ Assets/Phase 1/Camera/CameraController.cs
@@ -15,6 +15,9 @@ namespace Phase_1.Camera
         private Vector2 _movementDirection = Vector2.zero;
         private UnityEngine.Camera _camera;
 
+        private float _zoomSpeed = 10f;
+        private float _minZoom = 4f;
+        private float _maxZoom = 15f;
         private float _goalOrthographicSize = 5f;
         private bool _hasZoomed = false;
 
@@ -62,9 +65,18 @@ namespace Phase_1.Camera
 
         public void OnMove(InputValue inputValue)
         {
-            if (_phase == Phase.Escaping) return;
+            if (_phase != Phase.Building) return;
 
             _movementDirection = inputValue.Get<Vector2>();
+        }
+
+        public void OnZoom(InputValue inputValue)
+        {
+            if (_phase != Phase.Building) return;
+
+            var val = inputValue.Get<Vector2>().y / 60 * -1;
+            var goalSize = Math.Max(Math.Min(_camera.orthographicSize + val, _maxZoom), _minZoom);
+            _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, goalSize, Time.deltaTime * _zoomSpeed);
         }
 
         private void FollowPlayer()
