991f93a31b36effb5df97cd701bfeefeb5b7ba29
Alex Miller <alex.miller@readify.net>
2021-07-02 22:52:17 +1000

    Simple card choosing works

--- Assets/Phase 1/Builder/CardChooser/AttractionCard.cs
+++ Assets/Phase 1/Builder/CardChooser/AttractionCard.cs
@@ -3,39 +3,69 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AttractionCard : MonoBehaviour
+namespace Phase_1.Builder.CardChooser
 {
-    public Attraction attraction;
-    public TextMeshProUGUI nameText;
-    public TextMeshProUGUI costText;
-    public Image image;
-    public Image check;
-
-    private bool _isSelected = false;
-
-    
-    // Start is called before the first frame update
-    void Start()
+    public class AttractionCard : MonoBehaviour
     {
-        var monsterSpriteRenderer = attraction.monster.GetComponent<SpriteRenderer>();
-        image.color = monsterSpriteRenderer.color;
-        image.sprite = monsterSpriteRenderer.sprite;
-        image.preserveAspect = true;
+        public Attraction attraction;
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI costText;
+        public Image image;
+        public Image check;
+        public Deck deck;
+        public bool isBigCard; // TODO: Allow clicking bigCard to deselect small card also
+        public bool isEmpty = true;
 
-        nameText.text = attraction.name;
-        costText.text = $"${attraction.cost}";
+        private readonly Color _defaultImageColour = new Color(0, 0, 0, 0);
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetAttraction(attraction);
         
-        Select(false);
-    }
+            SetSelected(false);
+        }
 
-    public void OnClick()
-    {
-        Select(!_isSelected);
-    }
+        public void SetAttraction(Attraction at)
+        {
+            isEmpty = at is null; 
+            var monsterSpriteRenderer = at is null ? null : at.monster.GetComponent<SpriteRenderer>();
+            image.color = monsterSpriteRenderer is null ? _defaultImageColour : monsterSpriteRenderer.color;
+            image.sprite = monsterSpriteRenderer is null ? null : monsterSpriteRenderer.sprite;
+            image.preserveAspect = true;
 
-    private void Select(bool set)
-    {
-        _isSelected = set;
-        check.gameObject.SetActive(_isSelected);
+            nameText.text = at is null ? "____" : at.name;
+            costText.text = at is null ? "$$$" : $"${at.cost}";
+
+            attraction = at;
+            if (at is null) SetSelected(false);
+        }
+
+        public void OnClick()
+        {
+            if (isBigCard) return;
+
+            var isChosen = deck.AttractionIsChosen(attraction);
+            if (isChosen)
+            {
+                deck.DeSelectAttraction(attraction);
+                SetSelected(false);
+            }
+            else if (deck.ChooseAttraction(attraction))
+            {
+                SetSelected(true);
+            }
+        }
+
+        private void SetSelected(bool set)
+        {
+            if (isBigCard)
+            {
+                check.gameObject.SetActive(false);
+            } else
+            {
+                check.gameObject.SetActive(set);
+            }
+        }
     }
 }
---
+++ Assets/Phase 1/Builder/CardChooser/Deck.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using Phase_1.Builder.Buildings;
+using UnityEngine;
+
+namespace Phase_1.Builder.CardChooser
+{
+    public class Deck : MonoBehaviour
+    {
+        private const int MaxChosen = 2;
+        public List<AttractionCard> chosen = new List<AttractionCard>(MaxChosen);
+
+        public bool AttractionIsChosen(Attraction attraction)
+        {
+            return chosen.Exists(c => !(c is null) && !c.isEmpty && c.attraction.name == attraction.name); // TODO: Checking on name is bad
+        }
+
+        public void DeSelectAttraction(Attraction attraction)
+        {
+            if (AttractionIsChosen(attraction))
+            {
+                chosen.First(c => !c.isEmpty && c.attraction.name == attraction.name).SetAttraction(null); // TODO: Checking on name is bad
+            }
+        }
+
+        private bool IsFull()
+        {
+            return chosen.All(c => !c.isEmpty);
+        }
+
+        /// <summary>
+        /// Add attraction to your deck
+        /// </summary>
+        /// <param name="attraction"></param>
+        /// <returns>True if attraction was selected</returns>
+        public bool ChooseAttraction(Attraction attraction)
+        {
+            if (IsFull()) return false;
+
+            chosen.First(c => c.isEmpty).SetAttraction(attraction);
+            return true;
+        }
+    }
+}
