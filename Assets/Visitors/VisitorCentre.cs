216d6a69408ff43b8f4e4f290989cc41fc790b18
Alex Miller <alex.miller@readify.net>
2021-07-06 20:56:00 +1000

    Don't spawn anymore visitors after the park breaks

--- Assets/Visitors/VisitorCentre.cs
+++ Assets/Visitors/VisitorCentre.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using GameManagement;
 using UnityEngine;
 using Utilities;
 using Utilities.Extensions;
@@ -9,13 +10,29 @@ namespace Visitors
     {
         public float spawnChance;
         public List<GameObject> visitorBag;
+        private bool _parkIsBroken = false;
+
+        private void Start()
+        {
+            FindObjectOfType<GameManager>().OnPhaseChanged += OnPhaseChanged;
+        }
+
+        private void OnPhaseChanged(object sender, Phase newPhase)
+        {
+            if (newPhase == Phase.RunningFromDinosaurs)
+            {
+                _parkIsBroken = true;
+            }
+        }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            if (_parkIsBroken) return;
+
             if (MyRandom.CoinFlip(spawnChance))
             {
-                var visitor  =visitorBag.RandomChoice();
+                var visitor= visitorBag.RandomChoice();
                 Instantiate(visitor, transform.position, Quaternion.identity);
             }
         }
