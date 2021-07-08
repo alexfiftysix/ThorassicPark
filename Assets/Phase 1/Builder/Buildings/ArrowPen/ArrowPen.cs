da35f6d17f4d3cd1e4d88e761f7456195b56d743
Alex Miller <alex.miller@readify.net>
2021-07-08 22:26:18 +1000

    Move all general code from ArrowPen to Attraction

--- Assets/Phase 1/Builder/Buildings/ArrowPen/ArrowPen.cs
+++ Assets/Phase 1/Builder/Buildings/ArrowPen/ArrowPen.cs
@@ -1,30 +1,6 @@
-using Utilities;
-using Utilities.Extensions;
-
 namespace Phase_1.Builder.Buildings.ArrowPen
 {
     public class ArrowPen : Attraction
     {
-        private const float MoneyTime = 5;
-        private Timer _moneyTimer;
-
-        private void Start()
-        {
-            breakChancePercent = 2;
-            isBroken = false;
-            _moneyTimer = gameObject.AddTimer(MoneyTime, AddMoney);
-        }
-
-        private void AddMoney()
-        {
-            // TODO: Move this money logic to its own script
-            moneyBag.AddMoney(viewRadius.VisitorCount);
-        }
-
-        protected override void Break()
-        {
-            base.Break();
-            Destroy(_moneyTimer);
-        }
     }
 }
