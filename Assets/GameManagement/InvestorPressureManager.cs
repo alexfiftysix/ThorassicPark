664c87560392906b865cf605fcaa94c04d419498
Alex Miller <alex.miller@readify.net>
2021-07-11 12:32:47 +1000

    Park breaks after 5 seconds of low growth

--- Assets/GameManagement/InvestorPressureManager.cs
+++ Assets/GameManagement/InvestorPressureManager.cs
@@ -10,29 +10,40 @@ namespace GameManagement
 {
     public class InvestorPressureManager : MonoBehaviour
     {
+        private GameManager _gameManager;
+
         private float _intervalInSeconds = 1;
         [SerializeField] private float sliderMoveSpeed = 1;
         [SerializeField] private float sliderGrowthSpeed = 1;
 
         private float _moneyAtLastInterval;
-        private Timer _moneyTimer;
+        private Timer _calculationTimer;
 
         public Slider slider;
-        public TextMeshProUGUI earningsChangePerSecond;
         private float _dollarsPerSecond;
         private float _highestDollarsPerSecond;
 
+        public TextMeshProUGUI growthPerSecondText;
         private float _lastDollarsPerSecond = 0;
         private float _growthPerSecond = 0;
         private float _highestGrowthPerSecond = 1;
         private float _lowestGrowthPerSecond = -1;
 
+        [SerializeField] private float lowGrowthMaximumTime = 5;
+        [SerializeField] private float lowGrowthThreshold = 1;
+        private Timer _lowGrowthTimer;
+
         // Start is called before the first frame update
         private void Start()
         {
             _moneyAtLastInterval = MyStatistics.moneyEarned;
-            _moneyTimer = gameObject.AddTimer(_intervalInSeconds, CalculateDollarsPerSecond);
-            GetComponent<GameManager>().OnParkBreaks += (sender, args) => OnParkBreaks();
+            _calculationTimer = gameObject.AddTimer(_intervalInSeconds, Recalculate);
+
+            _gameManager = GetComponent<GameManager>();
+            _gameManager.OnParkBreaks += (sender, args) => OnParkBreaks();
+
+            _lowGrowthTimer = gameObject.AddTimer(lowGrowthMaximumTime, InvestorShutdown);
+            _lowGrowthTimer.DeActivate();
         }
 
         private void Update()
@@ -45,7 +56,42 @@ namespace GameManagement
         private void OnParkBreaks()
         {
             Destroy(slider);
-            Destroy(_moneyTimer);
+            Destroy(_calculationTimer);
+            ExitLowGrowthZone();
+        }
+
+        private void Recalculate()
+        {
+            CalculateDollarsPerSecond();
+
+            CalculateGrowthPerSecond();
+            
+            if (!_lowGrowthTimer.isActive && _growthPerSecond < lowGrowthThreshold)
+            {
+                EnterLowGrowthZone();
+            } 
+            else if (_lowGrowthTimer.isActive && _growthPerSecond >= lowGrowthThreshold)
+            {
+                ExitLowGrowthZone();
+            }
+        }
+
+        private void EnterLowGrowthZone()
+        {
+            _lowGrowthTimer.Activate();
+
+            var colours = ColorBlock.defaultColorBlock;
+            colours.normalColor = Color.red;
+            slider.colors = colours;
+        }
+
+        private void ExitLowGrowthZone()
+        {
+            _lowGrowthTimer.DeActivate();
+
+            var colours = ColorBlock.defaultColorBlock;
+            colours.normalColor = Color.white;
+            slider.colors = colours;
         }
 
         private void CalculateDollarsPerSecond()
@@ -53,13 +99,22 @@ namespace GameManagement
             _dollarsPerSecond = (MyStatistics.moneyEarned - _moneyAtLastInterval) / _intervalInSeconds;
             _moneyAtLastInterval = MyStatistics.moneyEarned;
             _highestDollarsPerSecond = Math.Max(_highestDollarsPerSecond, _dollarsPerSecond);
+        }
 
+        private void CalculateGrowthPerSecond()
+        {
             _growthPerSecond = _dollarsPerSecond - _lastDollarsPerSecond;
             _lastDollarsPerSecond = _dollarsPerSecond;
             _highestGrowthPerSecond = Math.Max(_highestGrowthPerSecond, _growthPerSecond);
             _lowestGrowthPerSecond = Math.Min(_lowestGrowthPerSecond, _growthPerSecond);
 
-            earningsChangePerSecond.text = $"{_growthPerSecond:C}";
+            growthPerSecondText.text = $"{_growthPerSecond:C}";
+        }
+
+
+        private void InvestorShutdown()
+        {
+            _gameManager.InvestorShutDown();
         }
     }
 }
