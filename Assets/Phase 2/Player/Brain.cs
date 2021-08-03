c885daf5ecfb6d96d53411e055a496f9c6587062
Alex Miller <alex.miller@readify.net>
2021-08-03 20:59:46 +1000

    Add basic Brain
    - controls an IMoveable

---
+++ Assets/Phase 2/Player/Brain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Utilities;
+using Utilities.Extensions;
+
+namespace Phase_2.Player
+{
+    public class Brain : MonoBehaviour
+    {
+        private IMoveable _moveable;
+        private Vector2 _direction;
+
+        public void Start()
+        {
+            _moveable = GetComponent<IMoveable>();
+            _direction = Vector2.zero;
+            gameObject.AddTimer(new RangedFloat(1, 3), Turn);
+        }
+
+        public void Update()
+        {
+            _moveable.Move(_direction);
+        }
+
+        private void Turn()
+        {
+            _direction = Directions.directions.RandomChoice();
+        }
+    }
+}
